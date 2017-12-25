4b0981a2cae8ac731eb1eda54773d545a5f48328
Антон Кудрин <antonkudrin25@gmail.com>
2017-12-25 22:34:58 +0300

    Add filtration

--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -20,13 +20,43 @@ namespace MVC.Controllers
         }
 
         [Authorize(Roles = "user")]
-        public ActionResult ShowAllSales()
+        public ActionResult ShowAllSales(int? manager, string product )
         {
             using (IBridgeToBLL db = new BridgeToBLL())
             {
-                ViewBag.Sales = db.GetSales();
+                IEnumerable<SaleViewModel> sales = db.GetSales();
+                IList<ManagerViewModel> managers = db.GetManagers().ToList();
+                managers.Insert(0, new ManagerViewModel { ManagerID = 0, LastName="Все" } );
+
+                DateTime date = new DateTime();
+                if (manager!=0 && manager!=null)
+                {
+                    sales = sales.Where(x =>x.ManagerID == manager);
+                    
+                }
+                if (!String.IsNullOrEmpty(product) && !product.Equals("Любой"))
+                {
+                    sales = sales.Where(x => x.Product == product);
+                }
+                //if(date != null)
+                //{
+                //    sales = sales.Where(x => x.Date.Day == date.Day && x.Date.Month == date.Month && x.Date.Year == date.Year);
+                //}
+                IList<string> products = sales.Select(x=>x.Product).Distinct().ToList();
+                products.Insert(0, "Любой");
+                IList<DateTime> dates = sales.Select(x => x.Date).Distinct().ToList();
+
+                //ViewBag.Sales = db.GetSales();
+                FilterModel filter = new FilterModel
+                {
+                    Sales = sales,
+                    Managers = new SelectList(managers, "ManagerId", "LastName"),
+                    Products = new SelectList(products),
+                    Dates = new SelectList(dates)
+                };
+                return View(filter);
             }
-            return View();
+            
         }
 
         // Get : CreateUser
@@ -64,12 +94,36 @@ namespace MVC.Controllers
             }
         }
 
-        [Authorize(Roles = "user")]
-        public ActionResult ShowAllManagers()
+        [HttpPost]
+        public ActionResult ProductSearch(string product)
         {
             using (IBridgeToBLL db = new BridgeToBLL())
             {
+
+                return PartialView(db.FilterByProduct(product));
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DateSearch(DateTime date)
+        {
+            using (IBridgeToBLL db = new BridgeToBLL())
+            {
+
+                return PartialView(db.FilterByDate(date));
+
+            }
+        }
+
+        [Authorize(Roles = "user")]
+        public ActionResult ShowAllManagers()
+        {
+
+            using (IBridgeToBLL db = new BridgeToBLL())
+            {
                 ViewBag.Managers = db.GetManagers();
+                
             }
             return View();
         }
---
+++ MVC/Models/FilterModels.cs
@@ -0,0 +1,17 @@
+﻿using PresentationLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Models
+{
+    public class FilterModel
+    {
+        public IEnumerable<SaleViewModel> Sales { get; set; }
+        public SelectList Managers { get; set; }
+        public SelectList Products { get; set; }
+        public SelectList Dates { get; set; }
+    }
+}
