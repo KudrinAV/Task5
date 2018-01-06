9e876c575cc78056118fc08a9dbe0d664d064ccc
Антон Кудрин <antonkudrin25@gmail.com>
2018-01-07 01:39:43 +0300

    fixed redirect

--- MVC/Controllers/UserController.cs
+++ MVC/Controllers/UserController.cs
@@ -99,7 +99,7 @@ namespace MVC.Controllers
                 using (IBridgeToBLL db = new BridgeToBLL())
                 {
 
-                    if (db.CreateSale(new SaleViewModel(DateTime.Now, model.Client, model.Product, model.Price, db.GetManagerIdForUser(User.Identity.Name)))) return RedirectToAction("ShowAllSales", "Admin");
+                    if (db.CreateSale(new SaleViewModel(DateTime.Now, model.Client, model.Product, model.Price, db.GetManagerIdForUser(User.Identity.Name)))) return RedirectToAction("ShowAllSales", "User");
                     else ModelState.AddModelError("", "Пользователя с таким логином уже есть");
                 }
             }
